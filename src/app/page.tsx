00a40454480a55b8e049f07105934488f0f96869
Anuj <anujjoshi63@gmail.com>
2023-11-22 00:03:37 -0700

    add work-ex chronological layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -197,8 +197,7 @@ export default function Home() {
 							work experience
 						</h2>
 					</div>
-
-					<div className="flex flex-col w-full gap-4 flex-wrap">
+					{/* <div className="flex flex-col w-full gap-4 flex-wrap">
 						<div className="flex flex-col items-center shadow-xl rounded-xl p-6 gap-2 flex-1 bg-zinc-800 min-w-[15rem] w-full">
 							<h1 className="text-2xl font-semibold">KyloApps</h1>
 							<div className="flex flex-col items-start text-lg mt-2">
@@ -218,7 +217,7 @@ export default function Home() {
 								<div>Next.js</div>
 								<div>Express</div>
 								<div>Electron</div>
-								{/* <div>React Native</div> */}
+								<div>React Native</div>
 								<div>Node.js</div>
 								<div>and more</div>
 							</div>
@@ -233,6 +232,144 @@ export default function Home() {
 								<div>Figma</div>
 								<div>AWS</div>
 								<div>Linux</div>
+							</div>
+						</div>
+					</div> */}
+					<div className="container px-5 mx-auto flex flex-wrap">
+						<div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+							<div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+								<div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+							</div>
+							<div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-zinc-500 text-white relative z-10 title-font text-sm ">
+								1
+							</div>
+							<div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+								<div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-950 rounded-full inline-flex items-center justify-center">
+									Dev
+								</div>
+								<div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+									<h2 className="font-medium title-font text-zinc-300 mb-1 text-3xl tracking-tight">
+										Auto Shop Insights - Freelance
+									</h2>
+									<p className="leading-relaxed">
+										Lorem ipsum dolor sit amet, consectetur
+										adipisicing elit. Voluptate, repellendus
+										ratione vel ipsam ab cumque in, optio
+										minus, nostrum quo similique voluptatem
+										non odit nisi? Illum unde veritatis
+										obcaecati praesentium!
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+							<div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+								<div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+							</div>
+							<div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-zinc-500 text-white relative z-10 title-font text-sm ">
+								2
+							</div>
+							<div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+								<div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-950 rounded-full inline-flex items-center justify-center">
+									<svg
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										className="w-12 h-12"
+										viewBox="0 0 24 24"
+									>
+										<path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+									</svg>
+								</div>
+								<div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+									<h2 className="font-medium title-font text-zinc-300 mb-1 text-3xl tracking-tight">
+										KyloApps - Internship
+									</h2>
+									<p className="leading-relaxed">
+										Lorem ipsum dolor sit amet, consectetur
+										adipisicing elit. Voluptate, repellendus
+										ratione vel ipsam ab cumque in, optio
+										minus, nostrum quo similique voluptatem
+										non odit nisi? Illum unde veritatis
+										obcaecati praesentium!
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+							<div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+								<div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+							</div>
+							<div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-zinc-500 text-white relative z-10 title-font text-sm ">
+								3
+							</div>
+							<div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+								<div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-950 rounded-full inline-flex items-center justify-center">
+									<svg
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										className="w-12 h-12"
+										viewBox="0 0 24 24"
+									>
+										<circle cx="12" cy="5" r="3"></circle>
+										<path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+									</svg>
+								</div>
+								<div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+									<h2 className="font-medium title-font text-zinc-300 mb-1 text-3xl tracking-tight">
+										TechFryDay - Internship
+									</h2>
+									<p className="leading-relaxed">
+										Lorem ipsum dolor sit amet, consectetur
+										adipisicing elit. Voluptate, repellendus
+										ratione vel ipsam ab cumque in, optio
+										minus, nostrum quo similique voluptatem
+										non odit nisi? Illum unde veritatis
+										obcaecati praesentium!
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+							<div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+								<div className="h-full w-1 bg-gray-300 pointer-events-none"></div>
+							</div>
+							<div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-zinc-500 text-white relative z-10 title-font text-sm ">
+								4
+							</div>
+							<div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+								<div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-950 rounded-full inline-flex items-center justify-center">
+									<svg
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										className="w-12 h-12"
+										viewBox="0 0 24 24"
+									>
+										<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+										<circle cx="12" cy="7" r="4"></circle>
+									</svg>
+								</div>
+								<div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+									<h2 className="font-medium title-font text-zinc-300 mb-1 text-3xl tracking-tight">
+										MIS Software - Freelance
+									</h2>
+									<p className="leading-relaxed">
+										Lorem ipsum dolor sit amet, consectetur
+										adipisicing elit. Voluptate, repellendus
+										ratione vel ipsam ab cumque in, optio
+										minus, nostrum quo similique voluptatem
+										non odit nisi? Illum unde veritatis
+										obcaecati praesentium!
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>
