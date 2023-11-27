e50c2df5dee933f16688616646c31cd7d642b200
Anuj <anujjoshi63@gmail.com>
2023-11-26 19:23:58 -0700

    refactor: show intro CTA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,26 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <section key="1" className="w-full py-12 md:py-24 lg:py-32">
+        <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-6">
           <Intro />
 
-          {/* <div className="flex items-center justify-center gap-4 w-full font-light text-lg text-zinc-300">
-						<Link
-							href={'https://github.com/anujjoshi63'}
-							target="_blank"
-						>
-							<div className="underline underline-offset-4">
-								GitHub
-							</div>
-						</Link>
-						<Link
-							href={'https://www.linkedin.com/in/thejoshi/'}
-							target="_blank"
-						>
-							<div className="underline underline-offset-4">
-								LinkedIn
-							</div>
-						</Link>
-
-						<Link
-							href={'https://www.instagram.com/anuj._.joshi/'}
-							target="_blank"
-						>
-							<div className="underline underline-offset-4">
-								Resume
-							</div>
-						</Link>
-					</div> */}
+          <div className="mt-4 flex w-full items-center justify-center gap-4 text-lg font-light text-zinc-300 ">
+            <Link
+              href={"https://www.linkedin.com/in/thejoshi/"}
+              target="_blank"
+            >
+              <div className="underline underline-offset-4">LinkedIn</div>
+            </Link>
+            <Link href={"https://github.com/anujjoshi63"} target="_blank">
+              <div className="underline underline-offset-4">GitHub</div>
+            </Link>
+            <Link
+              href={"https://www.instagram.com/anuj._.joshi/"}
+              target="_blank"
+            >
+              <div className="underline underline-offset-4">Resume</div>
+            </Link>
+          </div>
           <div className="space-y-3">
             <h2 className="mt-20 font-semibold tracking-tight sm:text-3xl md:text-4xl lg:text-5xl">
               highlights
