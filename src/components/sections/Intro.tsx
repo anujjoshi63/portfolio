6de8127deb7de5df9146423c879e64c14fffb0e1
Anuj <anujjoshi63@gmail.com>
2023-11-26 19:21:53 -0700

    fix: correct color and spacing issues in intro

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -2,19 +2,30 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <div className="space-y-4">
+    <div className="space-y-6">
       <h2 className="text-6xl font-bold tracking-tight md:text-5xl lg:text-6xl">
         Anuj Joshi
       </h2>
-      <p className="mx-auto max-w-[700px] text-xl/tight font-medium text-zinc-500 md:text-xl/relaxed md:font-normal lg:text-xl/relaxed xl:text-xl/relaxed">
-        <span className="text-zinc-200">Software Engineer</span> with{" "}
-        <span className="text-zinc-200">Freelance</span> exp,{" "}
-        <span className="text-zinc-200">Open Source</span> contributions,{" "}
-        <span className="text-zinc-200">Internships</span> and{" "}
-        <span className="text-zinc-200"> projects</span>.{" "}
+      <p className="mx-auto max-w-[700px] text-xl/normal font-medium text-zinc-500 md:text-xl/relaxed md:font-normal lg:text-xl/relaxed xl:text-xl/relaxed">
+        <span className="font-semibold text-[hsl(220,10%,75%)]">
+          Software Engineer
+        </span>{" "}
+        with{" "}
+        <span className="font-semibold text-[hsl(220,10%,75%)]">Freelance</span>{" "}
+        exp,{" "}
+        <span className="font-semibold text-[hsl(220,10%,75%)]">
+          Open Source
+        </span>{" "}
+        contributions,{" "}
+        <span className="font-semibold text-[hsl(220,10%,75%)]">
+          Internships
+        </span>{" "}
+        and{" "}
+        <span className="font-semibold text-[hsl(220,10%,75%)]"> projects</span>
+        .{" "}
         <span className="whitespace-nowrap italic">
           – I like my code{" "}
-          <span className="text-zinc-200">
+          <span className="font-semibold text-[hsl(220,10%,75%)]">
             blazing fast
             {" ;)"}
           </span>
