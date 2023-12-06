d36baa70c46db91f2f81946be0aab6fb5d350574
Anuj <anujjoshi63@gmail.com>
2023-12-05 23:03:05 -0700

    fix: temporarily hide the image placeholders

--- src/components/elements/card.tsx
+++ src/components/elements/card.tsx
@@ -10,13 +10,13 @@ const Card = ({ title, subtitle, otherData, imgPath }: CardProps) => {
   return (
     <div>
       <div className="flex w-full min-w-[15rem] flex-1 flex-col items-center gap-2 rounded-xl bg-zinc-700 p-6 shadow-xl md:w-fit">
-        <Image
+        {/* <Image
           alt="project"
           src={imgPath}
           width={100}
           height={100}
           className="aspect-square rounded-lg"
-        />
+        /> */}
         <h1 className="whitespace-nowrap text-2xl font-bold">{title}</h1>
         <h4 className=" mt-2 whitespace-nowrap text-lg font-light">
           {subtitle}
