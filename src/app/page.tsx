bff5e9144e7b18f8b15e225bbc66651a16dc5d03
Anuj <anujjoshi63@gmail.com>
2024-09-25 21:58:07 -0700

    remove dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import Name from "@/components/IntroSection/Name";
 import {
   Footer,
@@ -9,29 +8,15 @@ import {
   Skills,
   WorkExperience,
 } from "@/components/sections/";
-import { MoonIcon, SunMediumIcon } from "lucide-react";
-import { useState } from "react";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div className={darkMode ? "dark" : ""}>
-      <div
-        className="fixed right-5 top-5 z-10 rounded-full bg-themeWhite p-1 shadow-sm transition-all duration-700 dark:bg-themeBlack dark:text-themeWhite"
-        onClick={() => setDarkMode((old) => !old)}
-        onKeyUp={
-          () => setDarkMode((old) => !old)
-        }
-      >
-        {darkMode ? <SunMediumIcon /> : <MoonIcon />}
-      </div>
-      <div
-        className="min-h-screen bg-themeWhite text-themeBlack dark:bg-themeBlack dark:text-themeWhite"
-      >
+    <div>
+      <div className="min-h-screen bg-themeWhite text-themeBlack dark:bg-themeBlack dark:text-themeWhite">
         <Header />
         <section key="introduction" className="w-full py-12 md:py-24 lg:py-32">
           <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-6">
-            <Intro >
+            <Intro>
               <Name />
             </Intro>
             <Highlights />
