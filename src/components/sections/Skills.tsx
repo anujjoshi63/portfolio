d0fceff24dbc06d70f4df7e00e4223c12c79c39b
Anuj <anujjoshi63@gmail.com>
2024-09-25 23:29:31 -0700

    update skills section

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,15 +1,35 @@
 "use client";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
 import { motion } from "framer-motion";
-import Link from "next/link";
 import type { Ref } from "react";
 import { forwardRef } from "react";
 
 const Skills = forwardRef((_, ref: Ref<HTMLDivElement>) => {
+  const skillsMap = {
+    Languages: ["JavaScript", "TypeScript", "Python", "SQL"],
+    "Front-End": [
+      "React",
+      "Next.js",
+      "React Native",
+      "Redux",
+      // "Electron",
+      "TailwindCSS",
+      "Material-UI",
+    ],
+    "Back-End": [
+      "Node.js",
+      "Express.js",
+      "PostgreSQL",
+      "Redis",
+      "LangChain",
+      // "MySQL",
+      // "Firebase",
+      // "OpenAI APIs",
+      // "RESTful API",
+      // "GraphQL",
+    ],
+    "DevOps & Tools": ["Git & Github", "AWS", "Docker", "Linux", "CI/CD"],
+  };
+
   return (
     <motion.div
       id="skills_section"
@@ -28,68 +48,24 @@ const Skills = forwardRef((_, ref: Ref<HTMLDivElement>) => {
             skills
           </div>
 
-          <div className="flex w-full flex-wrap justify-center gap-8">
-            <div className="flex w-fit min-w-[14rem]  flex-col items-center gap-2 rounded-xl border border-zinc-400 bg-themeWhite p-6 shadow-xl dark:border-zinc-700 dark:bg-zinc-800">
-              <div className="w-full text-left text-2xl font-semibold">
-                languages
-              </div>
-              <div className="flex w-full flex-col items-start text-lg">
-                <div className="w-full text-left">TypeScript</div>
-                <div className="w-full text-left">Python</div>
-                <div className="w-full text-left">JavaScript</div>
-                <div className="w-full text-left">SQL</div>
-                <div className="w-full text-left">HTML/CSS</div>
-              </div>
-            </div>
-            <div className="flex w-fit min-w-[14rem]  flex-col items-center gap-2 rounded-xl border border-zinc-400 bg-themeWhite p-6 shadow-xl dark:border-zinc-700 dark:bg-zinc-800">
-              <div className="w-full text-left text-2xl font-semibold">
-                frameworks
-              </div>
-              <div className="flex w-full flex-col items-start text-lg">
-                <div className="w-full text-left">React</div>
-                <div className="w-full text-left">Next.js</div>
-                <div className="w-full text-left">Express</div>
-                <div className="w-full text-left">Electron</div>
-                {/* <div className="w-full text-left">React Native</div> */}
-                <HoverCard openDelay={0} closeDelay={250}>
-                  <HoverCardTrigger className="cursor-default">
-                    Node.js
-                  </HoverCardTrigger>
-                  <HoverCardContent className="w-fit rounded-xl border-zinc-400 bg-themeWhite px-4 py-2 dark:border-zinc-700 dark:bg-zinc-900">
-                    I know, I know. It&apos;s a runtime.
-                  </HoverCardContent>
-                </HoverCard>
-              </div>
-            </div>
-            <div className="flex w-fit min-w-[14rem]  flex-col items-center gap-2 rounded-xl border border-zinc-400 bg-themeWhite p-6 shadow-xl dark:border-zinc-700 dark:bg-zinc-800">
-              <div className="w-full text-left text-2xl font-semibold">
-                others
-              </div>
-              <div className="flex w-full flex-col items-start text-lg">
-                <div className="w-full text-left">Figma</div>
-                <div className="w-full text-left">AWS</div>
-                <div className="w-full text-left">Linux</div>
-                <div className="flex w-full text-left">
-                  Git &
-                  <div>
-                    <HoverCard openDelay={0} closeDelay={250}>
-                      <HoverCardTrigger>
-                        <div className="cursor-default">&nbsp;GitHub</div>
-                      </HoverCardTrigger>
-                      <HoverCardContent className="w-fit rounded-xl border-zinc-400 bg-themeWhite px-4 py-2 dark:border-zinc-700 dark:bg-zinc-900">
-                        <Link
-                          href={"https://github.com/anujjoshi63"}
-                          className="underline"
-                          target="_blank"
-                        >
-                          Checkout my github here
-                        </Link>
-                      </HoverCardContent>
-                    </HoverCard>
-                  </div>
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            {Object.entries(skillsMap).map(([key, value]) => (
+              <div
+                key={key}
+                className="flex w-fit min-w-[14rem]  flex-col items-center gap-2 rounded-xl border border-zinc-400 bg-themeWhite p-6 shadow-xl dark:border-zinc-700 dark:bg-zinc-800"
+              >
+                <div className="w-full text-left text-2xl font-semibold">
+                  {key}
+                </div>
+                <div className="flex w-full flex-col items-start text-lg">
+                  {value.map((skill: string) => (
+                    <div key={skill} className="w-full text-left">
+                      {skill}
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
