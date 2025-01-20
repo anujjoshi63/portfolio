882ae4fc6d6c76f98f6084afb6e2868779d9234a
Anuj <anujjoshi63@gmail.com>
2025-01-20 15:13:52 -0700

    Refactor ProjectCard component to use Card structure for improved layout and styling

--- src/components/elements/ProjectCard.tsx
+++ src/components/elements/ProjectCard.tsx
@@ -1,5 +1,8 @@
 import { ArrowUpRight } from "lucide-react";
 import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
 
 type ProjectCardProps = {
   title: string;
@@ -8,22 +11,35 @@ type ProjectCardProps = {
 };
 
 const ProjectCard = ({ title, description, link }: ProjectCardProps) => (
-  <Link href={link} target="_blank" className="mt-3 w-fit">
-    <div className="ease flex w-fit min-w-[15rem] flex-1 flex-col items-center gap-3 rounded-xl border border-zinc-400 p-6 shadow-xl transition-all duration-300 hover:border-zinc-500 dark:border-zinc-700 dark:bg-zinc-800 dark:hover:drop-shadow-[0_0_2px_hsl(240,0%,40%)] md:hover:-translate-y-1">
-      <div className="gap flex w-full items-center text-left text-2xl font-semibold tracking-tighter">
-        {title}{" "}
-        <ArrowUpRight
-          size={20}
-          color="gray"
-          className="ml-[2px] mt-[4px] min-w-[14px]"
-        />
-      </div>
-      <div className="flex flex-col items-center text-lg">
-        <div className="w-full max-w-[18ch] flex-1 text-left">
+  <Link href={link} target="_blank" className="mt-3 block w-full">
+    <Card
+      className={cn(
+        "group w-full min-w-[20rem] border-zinc-300 transition-all duration-300 hover:-translate-y-1",
+        "hover:border-zinc-400 dark:hover:drop-shadow-[0_0_2px_hsl(240,0%,40%)]",
+      )}
+    >
+      <CardHeader className="w-full pb-2">
+        <CardTitle className="flex items-center text-2xl font-semibold tracking-tighter">
+          {title}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex min-w-fit flex-col items-start gap-4 text-left text-base text-gray-700 md:whitespace-nowrap">
           {description}
+          <Button
+            variant="outline"
+            className="border-zinc-300 group-hover:text-slate-900"
+          >
+            View project
+            <ArrowUpRight
+              size={20}
+              color="gray"
+              className="ml-[2px] mt-[4px] min-w-[14px] transition-all group-hover:ml-1 group-hover:mt-0 group-hover:stroke-slate-700"
+            />
+          </Button>
         </div>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   </Link>
 );
 
