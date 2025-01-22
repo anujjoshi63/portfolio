31fa52b0f51aedc6a4dc1151a3c5cb15d7f5468e
Anuj <anujjoshi63@gmail.com>
2025-01-22 15:54:28 -0700

    Refactor ProjectCard component: add optional GitHub icon and update button text for improved clarity

--- src/components/elements/ProjectCard.tsx
+++ src/components/elements/ProjectCard.tsx
@@ -1,26 +1,34 @@
-import { ArrowUpRight } from "lucide-react";
-import Link from "next/link";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { cn } from "@/lib/utils";
-import { Button } from "./button";
+import Link from "next/link"
+import { ArrowUpRight, GithubIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { Button } from "./button"
 
 type ProjectCardProps = {
-  title: string;
-  description: string;
-  link: string;
-};
+  title: string
+  description: string
+  link: string
+  github?: boolean
+}
 
-const ProjectCard = ({ title, description, link }: ProjectCardProps) => (
+const ProjectCard = ({
+  title,
+  description,
+  link,
+  github,
+}: ProjectCardProps) => (
   <Link href={link} target="_blank" className="mt-3 block w-full">
     <Card
       className={cn(
         "group w-full min-w-[20rem] border-zinc-300 transition-all duration-300 hover:-translate-y-1",
-        "hover:border-zinc-400 dark:hover:drop-shadow-[0_0_2px_hsl(240,0%,40%)]",
+        "hover:border-zinc-400 dark:hover:drop-shadow-[0_0_2px_hsl(240,0%,40%)]"
       )}
     >
       <CardHeader className="w-full pb-2">
-        <CardTitle className="flex items-center text-2xl font-semibold tracking-tighter">
-          {title}
+        <CardTitle className="flex items-center gap-2 text-2xl font-semibold tracking-tighter">
+          {title} {github && <GithubIcon className="h-5 w-5" />}
         </CardTitle>
       </CardHeader>
       <CardContent>
@@ -28,19 +36,19 @@ const ProjectCard = ({ title, description, link }: ProjectCardProps) => (
           {description}
           <Button
             variant="outline"
-            className="border-zinc-300 group-hover:text-slate-900"
+            className="flex items-center border-zinc-300 group-hover:text-slate-900"
           >
-            View project
+            View {github ? "GitHub" : "Project"}
             <ArrowUpRight
               size={20}
               color="gray"
-              className="ml-[2px] mt-[4px] min-w-[14px] transition-all group-hover:ml-1 group-hover:mt-0 group-hover:stroke-slate-700"
+              className="ml-[2px] min-w-[14px] transition-all group-hover:-mt-1 group-hover:ml-1 group-hover:stroke-slate-700"
             />
           </Button>
         </div>
       </CardContent>
     </Card>
   </Link>
-);
+)
 
-export default ProjectCard;
+export default ProjectCard
