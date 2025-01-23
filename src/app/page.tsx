2a98446cc0513b777303dfd1cc6df9b6f4064477
Anuj <anujjoshi63@gmail.com>
2025-01-23 12:21:24 -0700

    Refactor Home and Skills components: streamline JSX structure and improve styling consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,18 @@ import {
 
 export default function Home() {
   return (
-    <div>
-      <div className="min-h-screen bg-themeWhite text-themeBlack dark:bg-themeBlack dark:text-themeWhite">
-        <Header />
-        <section key="introduction" className="w-full py-16 md:py-28 lg:py-36">
-          <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-6">
-            <Intro />
-          </div>
-        </section>
+    <div className="min-h-screen bg-themeWhite text-themeBlack dark:bg-themeBlack dark:text-themeWhite">
+      <Header />
+      <section key="introduction" className="w-full py-16 md:py-28 lg:py-36">
+        <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-6">
+          <Intro />
+        </div>
+      </section>
 
-        <Skills />
-        <Projects />
-        <WorkExperience />
-        <Footer />
-      </div>
+      <Skills />
+      <Projects />
+      <WorkExperience />
+      <Footer />
     </div>
   )
 }
