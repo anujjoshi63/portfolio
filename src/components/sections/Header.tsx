7dc89553b9f7e972fe8df150573133eadc06d295
Anuj <anujjoshi63@gmail.com>
2025-01-24 14:37:19 -0700

    Refactor Header component: improve code readability and formatting

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,5 +1,7 @@
 "use client"
-import { motion } from "framer-motion";
+
+import { motion } from "framer-motion"
+
 // type HeaderProps = {
 //   workExperienceRef: RefObject<HTMLDivElement>;
 //   skillsRef: RefObject<HTMLDivElement>;
@@ -12,38 +14,36 @@ const SECTIONS_ID_MAP = {
   SKILLS: "skills_section",
   WORK: "work_section",
   INTRO: "intro_section",
-  FOOTER: "footer_section"
+  FOOTER: "footer_section",
 }
 const Header = () => {
   const scrollToRef = (sectionId: string) => {
-    document.getElementById(sectionId)?.scrollIntoView(
-      {
-        behavior: "smooth",
-        block: sectionId !== "work_section" ? "center" : "start"
-      }
-    )
+    document.getElementById(sectionId)?.scrollIntoView({
+      behavior: "smooth",
+      block: sectionId !== "work_section" ? "center" : "start",
+    })
   }
 
   const animateFooter = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 500));
+    await new Promise((resolve) => setTimeout(resolve, 500))
     const footer = document.getElementById(SECTIONS_ID_MAP.FOOTER)
     if (!footer) return
-    footer.classList.add("bg-slate-300");
-    footer.classList.add("dark:bg-slate-900");
-    footer.classList.add("scale-105");
+    footer.classList.add("bg-slate-300")
+    footer.classList.add("dark:bg-slate-900")
+    footer.classList.add("scale-105")
     setTimeout(() => {
-      footer.classList.remove("bg-slate-300");
-      footer.classList.remove("dark:bg-slate-900");
-      footer.classList.remove("scale-105");
-    }, 500);
-  };
+      footer.classList.remove("bg-slate-300")
+      footer.classList.remove("dark:bg-slate-900")
+      footer.classList.remove("scale-105")
+    }, 700)
+  }
 
   const Button = ({
     children,
     onClick,
   }: {
-    children: string;
-    onClick: () => void;
+    children: string
+    onClick: () => void
   }) => (
     <button
       className="lowercase blur-none transition-all duration-300 hover:-translate-y-[2px] hover:drop-shadow-[0_1px_2px_hsl(240,0%,30%)] dark:hover:text-[hsl(240,0%,95%)]"
@@ -52,10 +52,13 @@ const Header = () => {
     >
       {children}
     </button>
-  );
+  )
 
   return (
-    <div className="flex w-full items-center justify-center pt-4" id="header_section">
+    <div
+      className="flex w-full items-center justify-center pt-4"
+      id="header_section"
+    >
       <motion.div
         initial={{ top: "-3rem", opacity: 0, filter: "blur(20px)", scale: 0.3 }}
         animate={{
@@ -73,19 +76,23 @@ const Header = () => {
         className="fixed z-50 flex w-fit items-center justify-center gap-6 rounded-full border   border-[hsla(0,0%,30%,0.5)] bg-gradient-to-tr from-[hsla(260,20%,70%,0.3)] to-[hsla(250,20%,60%,0.3)] px-4 py-2 bg-blend-difference backdrop-blur-md dark:from-[hsla(260,40%,20%,0.3)] dark:to-[hsla(250,40%,10%,0.3)]
 md:top-6"
       >
-        <Button onClick={() => scrollToRef(SECTIONS_ID_MAP.INTRO)}>about</Button>
-        <Button onClick={() => scrollToRef(SECTIONS_ID_MAP.SKILLS)}>skills</Button>
+        <Button onClick={() => scrollToRef(SECTIONS_ID_MAP.INTRO)}>
+          about
+        </Button>
+        <Button onClick={() => scrollToRef(SECTIONS_ID_MAP.SKILLS)}>
+          skills
+        </Button>
         <Button onClick={() => scrollToRef(SECTIONS_ID_MAP.WORK)}>work</Button>
         <Button
           onClick={() => {
-            scrollToRef(SECTIONS_ID_MAP.FOOTER);
-            animateFooter();
+            scrollToRef(SECTIONS_ID_MAP.FOOTER)
+            animateFooter()
           }}
         >
           Links
         </Button>
       </motion.div>
     </div>
-  );
-};
-export default Header;
+  )
+}
+export default Header
