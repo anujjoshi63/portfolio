9001466527eaa3ff86b1785788c516a9201d66aa
Anuj <anujjoshi63@gmail.com>
2025-01-24 09:47:22 -0700

    Refactor Home component: adjust padding for introduction section to enhance layout consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,10 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-themeWhite text-themeBlack dark:bg-themeBlack dark:text-themeWhite">
       <Header />
-      <section key="introduction" className="w-full py-16 md:py-28 lg:py-36">
+      <section
+        key="introduction"
+        className="w-full pt-16 pb-10 md:pt-28 lg:pt-36 md:pb-20"
+      >
         <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-6">
           <Intro />
         </div>
