b6e8f9a689bbbfe96e2953d65986c0d04b586059
Anuj <anujjoshi63@gmail.com>
2025-01-24 14:15:27 -0700

    Refactor NotFound component: update styling and improve layout for better user experience

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
 
 export default function NotFound() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
-      <div className="border-b border-b-zinc-800 py-1 text-2xl font-bold">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-8">
+      <div className="border-b border-b-slate-800 py-1 text-2xl font-bold">
         404
       </div>
       <Image
@@ -16,10 +16,10 @@ export default function NotFound() {
       />
       <Link
         href="/"
-        className="rounded-md border border-zinc-700 bg-zinc-800 px-2 py-1 text-lg text-zinc-200 shadow-xl"
+        className="smooth-corners-sm  text-slate-800 px-4 py-2 text-lg bg-slate-300 shadow-xl"
       >
         back to portfolio {";)"}
       </Link>
     </div>
-  );
+  )
 }
